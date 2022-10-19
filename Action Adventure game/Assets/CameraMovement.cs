0398ee2521144ac09895f699a9f3c1c9fe8f4035
Shiva <patel_shiva23@hotmail.com>
2022-10-19 13:37:51 -0400

    Dungeon Layout plus room transition

--- Action Adventure game/Assets/CameraMovement.cs
+++ Action Adventure game/Assets/CameraMovement.cs
@@ -7,6 +7,9 @@ public class CameraMovement : MonoBehaviour
 
     public Transform player;
     public float camSpeed;
+    public Vector2 maxPos;
+    public Vector2 minPos;
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +23,10 @@ public class CameraMovement : MonoBehaviour
         if (transform.position != player.position)
         {
             Vector3 playerPosition = new Vector3 (player.position.x, player.position.y, transform.position.z);
+
+            playerPosition.x = Mathf.Clamp(playerPosition.x, minPos.x, maxPos.x);
+            playerPosition.y = Mathf.Clamp(playerPosition.y, minPos.y, maxPos.y);
+
             transform.position = Vector3.Lerp(transform.position, playerPosition, camSpeed);
         }
     }
---
+++ Action Adventure game/Assets/RoomMove.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomMove : MonoBehaviour
+{
+    public Vector2 cameraChangeMin;
+    public Vector2 cameraChangeMax;
+    public Vector3 playerChange;
+    private CameraMovement cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main.GetComponent<CameraMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            cam.minPos = cameraChangeMin;
+            cam.maxPos = cameraChangeMax;
+            other.transform.position += playerChange;
+        }
+    }
+
+}
